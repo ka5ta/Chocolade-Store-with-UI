4c878620c46d0b9664a4c58b2f74a5f84b25676e
ka5ta <55314237+ka5ta@users.noreply.github.com>
2022-06-02 20:28:02 +0200

    adding GTO class for BasketProduct for GET requests

---
+++ java/com/gridu/store/DTO/BasketProductGetDTO.java
@@ -0,0 +1,29 @@
+package com.gridu.store.DTO;
+
+
+import lombok.Data;
+
+import java.text.DecimalFormat;
+
+@Data
+public class BasketProductGetDTO implements BasketProductDTO {
+
+    private Long productId;
+    private Long accountId = 1L;
+    private String productName;
+    private double productPrice;
+    private Integer stockQuantity;
+    private Long basketProductId;
+    private Integer basketQuantity;
+    private String basketProductPrice;
+
+
+    public String getBasketProductPrice(){
+        if(this.basketQuantity != null){
+            double basketProductPrice = this.productPrice * this.basketQuantity;
+            this.basketProductPrice = String.format("%.2f PLN", basketProductPrice);
+        }
+        return this.basketProductPrice;
+    }
+
+}
